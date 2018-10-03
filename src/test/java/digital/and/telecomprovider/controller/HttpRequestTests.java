5428196ca349a35c4a042099f2b612dfb56579aa
Johannes Griesser <jkgriesser@gmail.com>
2018-10-03 15:27:17 +0100

    Add REST PhoneNumberController, ApplicationTest and HttpRequestTests

--- src/test/java/digital/and/telecomprovider/controller/HttpRequestTests.java
+++ src/test/java/digital/and/telecomprovider/controller/HttpRequestTests.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HttpRequestTest {
+public class HttpRequestTests {
 
     @LocalServerPort
     private int port;
