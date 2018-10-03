f27f5962c1225b2ec3b5fe89d96950b03baf1388
Johannes Griesser <jkgriesser@gmail.com>
2018-10-03 14:31:56 +0100

    Initial commit

---
+++ src/main/java/digital/and/telecomprovider/TelecomProviderApplication.java
@@ -0,0 +1,12 @@
+package digital.and.telecomprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomProviderApplication.class, args);
+	}
+}
---
+++ src/test/java/digital/and/telecomprovider/TelecomproviderApplicationTests.java
@@ -0,0 +1,16 @@
+package digital.and.telecomprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TelecomproviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
